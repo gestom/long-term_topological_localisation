9e71ad029fa4cf5875a62e3429a7275249da22ff
frementester <fremenrobotics@gmail.com>
2017-02-24 14:52:26 +0100

    Update benchmark.sh

--- src/scripts/benchmark.sh
+++ src/scripts/benchmark.sh
@@ -5,14 +5,14 @@ function extend_figure
 {
 	w=$(identify $1 |cut -f 3 -d ' '|cut -f 1 -d x)
 	h=$(identify $1 |cut -f 3 -d ' '|cut -f 2 -d x)
-	if [ $w -lt 500 ]; then	convert $1 -bordercolor white -border $(((500-$w)/2))x0 $1;fi
-	if [ $h -lt 500 ]; then	convert $1 -bordercolor white -border 0x$(((500-$h)/2)) $1;fi
-	convert $1 -resize 500x500 $1
+	if [ $w -lt 1000 ]; then	convert $1 -bordercolor white -border $(((1000-$w)/2))x0 $1;fi
+	if [ $h -lt 1000 ]; then	convert $1 -bordercolor white -border 0x$(((1000-$h)/2)) $1;fi
+	convert $1 -resize 1000x1000 $1
 	w=$(identify $1 |cut -f 3 -d ' '|cut -f 1 -d x)
 	h=$(identify $1 |cut -f 3 -d ' '|cut -f 2 -d x)
-	if [ $w -lt 500 ]; then	convert $1 -bordercolor white -border $(((500-$w)/2))x0 $1;fi
-	if [ $h -lt 500 ]; then	convert $1 -bordercolor white -border 0x$(((500-$h)/2)) $1;fi
-	convert $1 -resize 500x500 $1
+	if [ $w -lt 1000 ]; then	convert $1 -bordercolor white -border $(((1000-$w)/2))x0 $1;fi
+	if [ $h -lt 1000 ]; then	convert $1 -bordercolor white -border 0x$(((1000-$h)/2)) $1;fi
+	convert $1 -resize 1000x1000 $1
 }
 
 function create_graph
@@ -66,7 +66,7 @@ rm benchmarks/*
 grep Overall $REPORTS/*-new.txt|sed s/.*reports.//|sed s/\-.*\://  |sed s/^5\ .*// >benchmarks/$d/New_method.txt;
 
 create_graph |dot -Tpdf >benchmarks/$d.pdf
-convert -density 200 benchmarks/$d.pdf -trim -bordercolor white benchmarks/$d.png 
+convert -density 400 benchmarks/$d.pdf -trim -bordercolor white benchmarks/$d.png 
 extend_figure benchmarks/$d.png
 cat best.txt |cut -f 2,4 -d ' '|tr ' ' _|sed s/$/.txt/|sed s/^/..\\/results\\//
 cp scripts/draw_summary_skelet.gnu benchmarks/draw_summary.gnu
@@ -76,14 +76,14 @@ do
 done
 gnuplot benchmarks/draw_summary.gnu >benchmarks/graphs.fig
 fig2dev -Lpdf benchmarks/graphs.fig benchmarks/graphs.pdf
-convert -density 200 benchmarks/graphs.pdf benchmarks/graphs.png
+convert -density 400 benchmarks/graphs.pdf benchmarks/graphs.png
 extend_figure benchmarks/graphs.png 
-convert -size 1100x600 xc:white \
-	-draw 'Image src-over 25,100 500,500 'benchmarks/graphs.png'' \
-	-draw 'Image src-over 575,100 500,500 'benchmarks/$d.png'' \
-	-pointsize 32 \
-	-draw 'Text 100,40 "Performance of temporal models for topological localisation"' \
-	-pointsize 18 \
+convert -size 2200x1200 xc:white \
+	-draw 'Image src-over 50,200 1000,1000 'benchmarks/graphs.png'' \
+	-draw 'Image src-over 1150,200 1000,1000 'benchmarks/$d.png'' \
+	-pointsize 64 \
+	-draw 'Text 200,80 "Performance of temporal models for topological localisation"' \
+	-pointsize 36 \
 	-gravity North \
-	-draw 'Text 0,60 "Arrow A->B means that A performs statistically significantly better that B"' benchmarks/summary.png;
+	-draw 'Text 0,120 "Arrow A->B means that A performs statistically significantly better that B"' benchmarks/summary.png;
 cp benchmarks/summary.png  $d/summary/summary.png
